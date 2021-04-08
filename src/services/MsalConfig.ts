a345e93c10a6e6818bf5eadca65df26436e511ad
Proxy004 <paul.j.prenn@gmail.com>
2021-04-08 19:53:48 +0200

    🎨 Dotenv used

--- src/services/MsalConfig.ts
+++ src/services/MsalConfig.ts
@@ -4,7 +4,7 @@ const msalConfig: Configuration = {
   auth: {
     clientId: process.env.REACT_APP_MICROSOFT_ID || "",
     //authority: `https://login.microsoftonline.com/${process.env.REACT_APP_AZURE_AD_TENANT_NAME}/`,
-    redirectUri: "http://localhost/:3000",
+    redirectUri: process.env.REACT_APP_REDIRECT_URI,
   },
   cache: {
     cacheLocation: "localStorage",
