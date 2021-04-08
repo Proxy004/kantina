7dc3196824dcc6e9a0d43e631faa63a3e7538a6b
Proxy004 <paul.j.prenn@gmail.com>
2021-04-08 19:57:37 +0200

    ✅ Msal working

--- src/services/MsalConfig.ts
+++ src/services/MsalConfig.ts
@@ -4,7 +4,7 @@ const msalConfig: Configuration = {
   auth: {
     clientId: process.env.REACT_APP_MICROSOFT_ID || "",
     //authority: `https://login.microsoftonline.com/${process.env.REACT_APP_AZURE_AD_TENANT_NAME}/`,
-    redirectUri: process.env.REACT_APP_REDIRECT_URI,
+    //redirectUri: process.env.REACT_APP_REDIRECT_URI,
   },
   cache: {
     cacheLocation: "localStorage",
