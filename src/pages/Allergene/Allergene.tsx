20b13ec6e350f086f33eb1b6a959e8ce14c3bf42
SandroRothmund <s.rothmund@vol.at>
2021-05-27 12:10:44 +0200

    🔥 NavBar Bug fixes

--- src/pages/Allergene/Allergene.tsx
+++ src/pages/Allergene/Allergene.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import NavBar from "../../components/NavBar/NavBar";
 import Footer from "../../components/Footer/Footer";
 import "./Allergene.scss";
+import Allergens from "../../assets/allergene.jpg";
 
 const Allergene = () => {
   return (
@@ -9,24 +10,12 @@ const Allergene = () => {
       <NavBar />
       <div className="allAllergene">
         <span className="titleAllergene">Allergene</span>
-        <div className="listAllergene">
-          <ul>
-            <li>A: Gluten</li>
-            <li>B: Krebstiere</li>
-            <li>C: Eier von Geflügel</li>
-            <li>D: Fisch</li>
-            <li>E: Erdnüsse</li>
-            <li>F: Sojabohnen</li>
-            <li>G: Milch von Säugetieren</li>
-            <li>H: Schalenfrüchte</li>
-            <li>L: Sellerie</li>
-            <li>M: Senf</li>
-            <li>N: Sesamsamen</li>
-            <li>O: Schwefeloxid und Sulfite</li>
-            <li>P: Lupinen</li>
-            <li>R: Weichtiere</li>
-          </ul>
-        </div>
+
+        <img
+          src={Allergens}
+          alt={"Unsere Allergene"}
+          className={"pictureAllergene"}
+        />
       </div>
       <Footer />
     </>
